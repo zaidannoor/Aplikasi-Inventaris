a0809774a3c1fcb55e0a5710557edaa230047e3f
Zaidan Noor <zaidannoor06@gmail.com>
2023-08-30 18:45:41 +0700

    Membuat File page baru

---
+++ src/pages/jurusan/InventarisJurusanPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InventarisJurusanPage() {
+  return (
+    <section className='inventaris-jurusan-page'>
+        <h1 className="text-center">Inventaris Jurusan</h1>
+    </section>
+  )
+}
+
+export default InventarisJurusanPage
